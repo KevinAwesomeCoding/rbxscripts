4f0be29d02681af444fc71bfaa91c292d5210121
KevinAwesomeCoding <70450869+KevinAwesomeCoding@users.noreply.github.com>
2026-04-23 22:11:48 -0500

    Update PLACE_ID for Park A Car script

--- Redirectory.lua
+++ Redirectory.lua
@@ -1,4 +1,4 @@
-local PLACE_ID = 5747081107  -- replace with the actual Place ID of the game
+local PLACE_ID = 113720203996283 --Park A Car
 
 if game.PlaceId == PLACE_ID then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/KevinAwesomeCoding/rbxscripts/refs/heads/main/Park%20a%20Car%20Script.lua"))()
