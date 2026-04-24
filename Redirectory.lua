fc276ea915b4de6057c36cd7126f6e716e4b9897
KevinAwesomeCoding <70450869+KevinAwesomeCoding@users.noreply.github.com>
2026-04-23 22:08:07 -0500

    Add script loading for specific Place ID
    Load external script if the game Place ID matches.

---
+++ Redirectory.lua
@@ -0,0 +1,5 @@
+local PLACE_ID = 5747081107  -- replace with the actual Place ID of the game
+
+if game.PlaceId == PLACE_ID then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/KevinAwesomeCoding/rbxscripts/refs/heads/main/Park%20a%20Car%20Script.lua"))()
+end
